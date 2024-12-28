4906f2b17bc293cdeab63cc6aa0a67c05c00560c
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-12-28 12:40:26 -0800

    reorganize

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+
 const app = express();
 
-const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-const indexRouter = require("./routes/indexRouter");
-
 app.use("/", indexRouter);
 
 const port = process.env.PORT || 8080;
-
+// syntax for binding to both IPv4 and IPv6
 app.listen(port, "::", () => {
   console.log(`Server listening on [::]${port}`);
 });
